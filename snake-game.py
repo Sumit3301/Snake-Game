a96cf48ff7ac869501b71c201efc857213fa1220
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-07-22 01:16:36 +0530

    updated animatons of snake

--- snake-game.py
+++ snake-game.py
@@ -1,22 +1,31 @@
 #Writing snake game on python using turtle library
 
 from turtle import Turtle,Screen
+import time
 import turtle
 screen=Screen() #initilizing screen
 screen.setup(width=600,height=600)#defining screen size
 screen.title("Snake Game")
-
+screen.tracer(0)
 
 
 #Creating snake's body
 starting_pos=[(0,0),(-20,0),(-40,0)]
-
+segments=[]
 for position in starting_pos:
     new_segment=Turtle("square")
     new_segment.color("Black")
     new_segment.goto(position)
+    segments.append(new_segment)
 
 
+game_is_on=True
+#Animations of snake
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
+        screen.update()
+    time.sleep(1)    
 
 
 screen.exitonclick() #Screen will exit on clik on screen
