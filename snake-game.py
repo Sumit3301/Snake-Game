381a8ec0daae30d9cbcae19fec8382fbd1eb8f1b
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-07-28 15:43:14 +0530

    implemented oops

--- snake-game.py
+++ snake-game.py
@@ -1,5 +1,6 @@
 #Writing snake game on python using turtle library
 
+from snake import Snake
 from turtle import Turtle,Screen,goto
 import time
 import turtle
@@ -8,29 +9,18 @@ screen.setup(width=600,height=600)#defining screen size
 screen.title("Snake Game")
 screen.tracer(0)
 
+snake=Snake
 
 #Creating snake's body
-starting_pos=[(0,0),(-20,0),(-40,0)]
-segments=[]
-for position in starting_pos:
-    new_segment=Turtle("square")
-    new_segment.color("Black")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
 
 game_is_on=True
 #Animations of snake
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in range(len(segments)-1,0,-1):
-        new_x=segments[seg-1].xcor()
-        new_y=segments[seg-1].ycor()
-        segments[seg].goto(new_x,new_y)
-    segments[0].forward(20)
-    segments[0].left(90)
+    snake.move()
+
+    
     
 
 screen.exitonclick() #Screen will exit on clik on screen
--- snake.py
+++ snake.py
@@ -1,2 +1,31 @@
+STARTING_POSITIONS=[(0,0),(-20,0),(-40,0)]
+MOVE_DIST=20
+from turtle import Turtle
 class Snake():
+    def __init__(self):
+        self.segments=[]
+        self.create_snake()
+    
+
+    def create_snake():
+        for position in STARTING_POSITIONS:
+            new_segment=Turtle("square")
+            new_segment.color("Black")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        
+#Animations of snake
+        for seg in range(len(self.segments)-1,0,-1):
+            new_x=self.segments[seg-1].xcor()
+            new_y=self.segments[seg-1].ycor()
+            self.segments[seg].goto(new_x,new_y)
+        self.segments[0].forward(MOVE_DIST)
+    
+
+        
+
+
     
